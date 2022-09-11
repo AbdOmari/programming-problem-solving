0b228d4349674db46e47dfa4bef3f9f4d403951b
AbdOmari <108370942+AbdOmari@users.noreply.github.com>
2022-09-11 08:32:39 -0800

    Add files via upload

---
+++ 1-problem solving-data tybes.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1;
+	long long num2;
+	char char1;
+	float num3;
+	double num4;
+	cin >> num1 >> num2 >> char1 >> num3 >> num4;
+	cout << num1 << endl;
+	cout << num2 << endl;
+	cout << char1 << endl;
+	cout << num3 << endl;
+	cout << num4 << endl;
+}
