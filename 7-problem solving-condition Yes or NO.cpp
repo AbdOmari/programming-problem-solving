8c0e9e1db419538fee0b5a180dd85a967b8a22e6
AbdOmari <108370942+AbdOmari@users.noreply.github.com>
2022-09-11 16:00:04 -0800

    Add files via upload

---
+++ 7-problem solving-condition Yes or NO.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+/*
+if x>=y =====>Yes
+if x<y =====>No
+*/
+int main()
+{
+	int x, y;
+	cin >> x >> y;
+	if (x >= y)
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
+}
